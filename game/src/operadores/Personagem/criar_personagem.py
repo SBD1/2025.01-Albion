66d5a9a8a01cbe28fe10bc66ef9568df1fc70e67
SAnjos3 <academicanjos@gmail.com>
2025-06-16 21:49:07 -0300

    fix/personagem: correção na função dml de inserção de novas tuplas de personagem

--- game/src/operadores/Personagem/criar_personagem.py
+++ game/src/operadores/Personagem/criar_personagem.py
@@ -1,23 +1,16 @@
 def criar_personagem(id_usuario, nome_personagem, especie_personagem, cursor):
     if cursor:
-        cursor.execute(f"SELECT f_cria_personagem({id_usuario}, '{nome_personagem}');")
-        id_personagem = cursor.fetchone()['f_cria_personagem']
-        # print(id_personagem)
-        if id_personagem is None:
-            print("❌ ERRO: Não foi possível criar personagem.\n")
+        try:
+            cursor.execute(f"SELECT f_cria_personagem({id_usuario}, '{nome_personagem}', '{especie_personagem}');")
+            id_personagem = cursor.fetchone()['f_cria_personagem']
+            
+            if id_personagem is None:
+                print("❌ ERRO: Não foi possível criar personagem.\n")
+                return False
+            
+            print(f"✅ Personagem criado com sucesso.")
+            print()
+            return True
+        except Exception as e:
+            print(f"❌ ERRO: {e}\n")
             return False
-        
-        if especie_personagem == 'Zoiudo':
-            cursor.execute(f"INSERT INTO public.ZOIUDO(id_personagem) VALUES ({id_personagem});")
-        elif especie_personagem == 'Draconico':
-            cursor.execute(f"INSERT INTO public.DRACONICO(id_personagem) VALUES ({id_personagem});")
-        elif especie_personagem == 'Espiritualista':
-            cursor.execute(f"INSERT INTO public.ESPIRITUALISTA(id_personagem) VALUES ({id_personagem});")
-        elif especie_personagem == 'Titan':
-            cursor.execute(f"INSERT INTO public.TITAN(id_personagem) VALUES ({id_personagem});")
-
-        print(f"✅ Personagem criado com sucesso.")
-        print()
-
-
-
--- migrations/V5.1__f_cria_personagem.sql
+++ migrations/V5.1__f_cria_personagem.sql
@@ -1,6 +1,7 @@
 CREATE OR REPLACE FUNCTION f_cria_personagem(
     p_id_usuario      INTEGER,
-    p_nome_personagem VARCHAR)
+    p_nome_personagem VARCHAR,
+    p_especie_personagem VARCHAR)
 RETURNS INTEGER AS $$
 
 DECLARE
@@ -10,6 +11,18 @@ BEGIN
     INSERT INTO public.personagem(id_usuario, nome)
     VALUES (p_id_usuario, p_nome_personagem)
     RETURNING id_personagem INTO v_id_personagem;
+
+    IF p_especie_personagem = 'Zoiudo' THEN
+        INSERT INTO public.ZOIUDO(id_personagem) VALUES (v_id_personagem);
+    ELSIF p_especie_personagem = 'Draconico' THEN
+        INSERT INTO public.DRACONICO(id_personagem) VALUES (v_id_personagem);
+    ELSIF p_especie_personagem = 'Espiritualista' THEN
+        INSERT INTO public.ESPIRITUALISTA(id_personagem) VALUES (v_id_personagem);
+    ELSIF p_especie_personagem = 'Titan' THEN
+        INSERT INTO public.TITAN(id_personagem) VALUES (v_id_personagem);
+    ELSE
+        RAISE EXCEPTION 'Espécie de personagem não reconhecida: %', p_especie_personagem;
+    END IF;
 
     RETURN v_id_personagem;
 END;
