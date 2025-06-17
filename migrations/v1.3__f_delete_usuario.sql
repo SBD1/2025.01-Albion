2c08bb31bd74e0b3739b449ec409db409ee078df
Davi <daviemanueloliveira7@gmail.com>
2025-06-16 21:51:02 -0300

    function de deletar  o usuário

---
+++ migrations/v1.3__f_delete_usuario.sql
@@ -0,0 +1,9 @@
+REATE OR REPLACE FUNCTION f_deleta_usuario(
+    p_username VARCHAR
+)
+RETURNS VOID AS $$
+BEGIN
+    DELETE FROM public.usuario
+    WHERE username = p_username;
+END;
+$$ LANGUAGE plpgsql;
