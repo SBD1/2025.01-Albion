a5f4641d8088e9baef34d66f8e3c25d6d9db6d24
leohssjr <leonardosauma.junior@gmail.com>
2025-07-05 02:48:17 -0300

    feat/logica-inventario: criando menu de ações básicas

---
+++ game/src/operadores/Personagem/menu_acoes.py
@@ -0,0 +1,31 @@
+from simple_term_menu import TerminalMenu
+from game.src.database import criar_cursor
+from game.src.limpar_tela import limpar_tela
+from game.src.ascii_art import salas
+from game.src.operadores.Sala.print_mapa import print_mapa
+
+def menu_acoes(id_personagem):
+    print_mapa(id_personagem)
+    opcoes = ["Mover", "Abrir Inventário","Sair"] # quando for fazer a lógica de atacar monstros colocar aqui uma opção "Atacar Monstro"
+    menu = TerminalMenu(
+        opcoes,
+        menu_cursor_style=("fg_green", "bold"),
+        menu_highlight_style=("fg_green", "bold"),
+        clear_screen=False
+    )
+
+    while True:
+        cursor = criar_cursor()
+        opcao = menu.show()
+        limpar_tela()
+
+        if opcao == 0:
+            limpar_tela()
+            return 'mover'
+        elif opcao == 1:
+            limpar_tela()
+            return "abrir inventário"
+            
+        elif opcao == 2:
+            limpar_tela()
+            return "sair"
