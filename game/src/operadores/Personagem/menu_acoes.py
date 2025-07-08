1d626635068be15e826c562f89d1646362f6bf5e
leohssjr <leonardosauma.junior@gmail.com>
2025-07-07 22:17:17 -0300

    feat/loja: atualizando o menu de ações

--- game/src/operadores/Personagem/menu_acoes.py
+++ game/src/operadores/Personagem/menu_acoes.py
@@ -6,7 +6,7 @@ from game.src.operadores.Sala.print_mapa import print_mapa
 
 def menu_acoes(id_personagem):
     print_mapa(id_personagem)
-    opcoes = ["Mover", "Abrir Inventário","Sair"] # quando for fazer a lógica de atacar monstros colocar aqui uma opção "Atacar Monstro"
+    opcoes = ["Mover", "Abrir Inventário","Visualizar Loja","Sair"] # quando for fazer a lógica de atacar monstros colocar aqui uma opção "Atacar Monstro"
     menu = TerminalMenu(
         opcoes,
         menu_cursor_style=("fg_green", "bold"),
@@ -18,6 +18,7 @@ def menu_acoes(id_personagem):
         cursor = criar_cursor()
         opcao = menu.show()
         limpar_tela()
+        print(opcao)
 
         if opcao == 0:
             limpar_tela()
@@ -27,5 +28,9 @@ def menu_acoes(id_personagem):
             return "abrir inventário"
             
         elif opcao == 2:
+            limpar_tela()
+            return "loja"
+        
+        elif opcao == 3:
             limpar_tela()
             return "sair"
