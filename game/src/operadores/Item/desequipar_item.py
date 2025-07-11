015d7ced382b70cef7641569523729613dbb0d13
SAnjos3 <academicanjos@gmail.com>
2025-07-11 14:25:15 -0300

    fix/feat: reajustar a função de desequipar para abranger mais casos de uso

--- game/src/operadores/Item/desequipar_item.py
+++
@@ -1,7 +0,0 @@
-def desequipar_item(cursor, id_instancia_item):
-    try:
-        cursor.execute(f"SELECT f_desequipar_item({id_instancia_item});")
-        cursor.connection.commit()
-        print("✅ Item desequipado com sucesso!")
-    except Exception as e:
-        print(f"❌ Erro ao desequipar item: {e}")
