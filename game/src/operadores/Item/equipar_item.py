2c40dced0c320eb33562b673314eca5c68342186
SAnjos3 <academicanjos@gmail.com>
2025-07-11 14:24:21 -0300

    feat/fix: ajustes realizados na função de equiparar para abranger mais funcionalidades

--- game/src/operadores/Item/equipar_item.py
+++
@@ -1,7 +0,0 @@
-def equipar_item(cursor, id_instancia_item):
-    try:
-        cursor.execute(f"SELECT f_equipar_item({id_instancia_item});")
-        cursor.connection.commit()
-        print("✅ Item equipado com sucesso!")
-    except Exception as e:
-        print(f"❌ Erro ao equipar item: {e}")
